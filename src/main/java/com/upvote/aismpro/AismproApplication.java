698d01fa46afb3ae43e1e09609259a9c630fa649
cheun0327 <cheun0327@gmail.com>
2021-08-18 11:18:29 +0900

    init spring boot

---
+++ src/main/java/com/upvote/aismpro/AismproApplication.java
@@ -0,0 +1,13 @@
+package com.upvote.aismpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AismproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AismproApplication.class, args);
+	}
+
+}
