7f40cbf075e06dce032b5f8112ae13d613767bbb
cheun0327 <cheun0327@gmail.com>
2021-08-18 12:05:49 +0900

    init spring boot

---
+++ src/test/java/com/upvote/aismpro/AismproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upvote.aismpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AismproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
