8e50cb806f6a82330e2b7f4b4dd0e9593a24d9b6
dev-whiteGyu <hana222966@naver.com>
2021-08-24 16:37:30 +0900

    210824 spring boot jenkins test

--- src/main/java/com/upvote/aismpro/controller/PageController.java
+++ src/main/java/com/upvote/aismpro/controller/PageController.java
@@ -1,15 +1,30 @@
 package com.upvote.aismpro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-public class PageController {
+@JsonComponent
+public class PageController  {
 
-    @GetMapping("/hello")
-    public String hello() {
+    private Object MapUtils;
+
+    @RequestMapping(value = "/hello", method = {RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestBody Map<String, Object> map) {
+        if (map == null || map.isEmpty()) System.out.println("empty");
+        else System.out.println("not null");
+
+        System.out.println(map);
+
         return "Hi 현재 서버 시간 : " + new Date() + "입니다. \n";
     }
 }
