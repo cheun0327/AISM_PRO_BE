220bb8e307ddc7fe4eb210eea5ecc3b2f97bf812
dev-whiteGyu <hana222966@naver.com>
2021-09-08 17:06:39 +0900

    security service init

--- src/main/java/com/upvote/aismpro/repository/UserRepository.java
+++ src/main/java/com/upvote/aismpro/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.upvote.aismpro.entity.BookEntity;
 import com.upvote.aismpro.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
