29b42e82a96d519af0561dde3fab5403d84f7bb1
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2021-09-28 15:28:45 +0900

    Update WebConfig.java

--- src/main/java/com/upvote/aismpro/settings/WebConfig.java
+++ src/main/java/com/upvote/aismpro/settings/WebConfig.java
@@ -6,10 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods("GET", "POST")
-//                .allowedOrigins("http://localhost:8888","http://localhost:3000");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST")
+                .allowedOrigins("http://localhost:8888","http://localhost:3000");
+    }
 }
