bf953ca30f0d875a2d668d308e506c0a2fd04518
dev-whiteGyu <hana222966@naver.com>
2021-09-28 16:27:06 +0900

    210928 Solve Book Problem

--- src/main/java/com/upvote/aismpro/repository/UserRepository.java
+++ src/main/java/com/upvote/aismpro/repository/UserRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     List<User> findByEmail(String email);
-    List<User> findByNickName(String nickName);}
+    List<User> findByNickName(String nickName);
+}
