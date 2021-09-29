102543aff98bd3815212acfb0bb5012f271d542c
dev-whiteGyu <hana222966@naver.com>
2021-09-29 14:33:52 +0900

    Library Page Return Value Remember Order

--- src/main/java/com/upvote/aismpro/repository/ComposeRepository.java
+++ src/main/java/com/upvote/aismpro/repository/ComposeRepository.java
@@ -3,6 +3,7 @@ package com.upvote.aismpro.repository;
 import com.upvote.aismpro.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,13 @@ public interface ComposeRepository extends JpaRepository<User, String> {
 
     @Query("select distinct compose.second_mood from Compose compose")
     List<String> findAllSecondMood();
+
+    @Query("select distinct compose.first_mood from Compose compose where compose.genre = :genre")
+    List<String> findFirstMoodByGenre(@Param("genre") String genre);
+
+    @Query("select distinct compose.second_mood from Compose compose where compose.genre = :genre and compose.first_mood = :firstMood")
+    List<String> findSecondMoodByFirstMood(@Param("genre") String genre, @Param("firstMood") String firstMood);
+
+    @Query("select distinct compose.sample from Compose compose where compose.genre = :genre and compose.first_mood = :firstMood and compose.second_mood = :secondMood")
+    String findSampleSoundByKeywords(@Param("genre") String genre, @Param("firstMood") String firstMood, @Param("secondMood") String secondMood);
 }
