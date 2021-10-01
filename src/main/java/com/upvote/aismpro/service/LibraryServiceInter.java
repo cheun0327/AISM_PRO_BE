eaf0d24c2262fc1037a2e871e1b4855c4cd3adf1
cheun0327 <cheun0327@gmail.com>
2021-10-01 18:07:37 +0900

    211001 Library Search POST

--- src/main/java/com/upvote/aismpro/service/LibraryServiceInter.java
+++ src/main/java/com/upvote/aismpro/service/LibraryServiceInter.java
@@ -2,7 +2,9 @@ package com.upvote.aismpro.service;
 
 import com.upvote.aismpro.dto.LibrarySearchDTO;
 
+import java.util.List;
+
 public interface LibraryServiceInter {
 
-    public void getSearchResult(LibrarySearchDTO librarySearchDto);
+    public List<String> getSearchResult(LibrarySearchDTO librarySearchDto);
 }
