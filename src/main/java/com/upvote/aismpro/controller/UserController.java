e615aff0aac92d84417ad221b9bc95870bbaeb27
cheun0327 <cheun0327@gmail.com>
2021-11-02 14:27:27 +0900

    Fix: user api url

--- src/main/java/com/upvote/aismpro/controller/UserController.java
+++ src/main/java/com/upvote/aismpro/controller/UserController.java
@@ -1,30 +1,28 @@
 package com.upvote.aismpro.controller;
 
-import com.upvote.aismpro.dto.LoginUserDTO;
+import com.upvote.aismpro.dto.UserDTO;
+import com.upvote.aismpro.entity.OAuth;
 import com.upvote.aismpro.entity.User;
 import com.upvote.aismpro.security.SecurityService;
+import com.upvote.aismpro.service.MyPageService;
 import com.upvote.aismpro.service.SignupServiceInter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
+
 
 @RestController
-public class SignupController {
-    @Autowired
-    private SignupServiceInter signupService;
+public class UserController {
 
     @Autowired
-    private SecurityService securityService;
+    private MyPageService mypageService;
+
+    @Autowired
+    private SignupServiceInter signupService;
 
     // 이메일 중복 확인
     @GetMapping("/user/email/validate/{email}")
@@ -56,32 +54,15 @@ public class SignupController {
         }
     }
 
-
-    @PostMapping("/signup/{nickName}")
-    public ResponseEntity<LoginUserDTO> signup(HttpServletRequest request, @PathVariable("nickName") String nickName) {
+    // user 정보 업데이트
+    @PatchMapping("/user")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO user) {
         try {
-            HttpSession session = request.getSession();
-            String platform = session.getAttribute("platform").toString();
-
-            User user = new User(
-                    nickName,
-                    session.getAttribute("email").toString(),
-                    platform
-            );
-
-            signupService.signup(user);
-
-             //user token 생성
-            String token = securityService.createToken(securityService.transformUserToJwtRequestDto(user));
-            // 로그인 정보 전달 DTO 생성
-            LoginUserDTO loginUser = new LoginUserDTO(token, user, platform);
-
-            return new ResponseEntity<>(loginUser, HttpStatus.OK);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            UserDTO updateUser = mypageService.updateUser(user.getUserId(), user);
+            return new ResponseEntity<>(updateUser, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
