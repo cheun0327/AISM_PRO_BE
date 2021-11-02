9d2d3f82bebe851257f7bc70c8f17fc86fe9578e
cheun0327 <cheun0327@gmail.com>
2021-11-02 14:29:44 +0900

    Fix: user api url

--- src/main/java/com/upvote/aismpro/controller/SignupController.java
+++ src/main/java/com/upvote/aismpro/controller/SignupController.java
@@ -26,37 +26,6 @@ public class SignupController {
     @Autowired
     private SecurityService securityService;
 
-    // 이메일 중복 확인
-    @GetMapping("/user/email/validate/{email}")
-    public @ResponseBody
-    Map<String, Boolean> emailDoubleCheck(@PathVariable("email") String email) {
-        System.out.println("== email Double Check : " + email);
-        try {
-            signupService.emailDoubleCheck(email);
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            return Collections.singletonMap("result", false);
-        }
-        return Collections.singletonMap("result", true);
-    }
-
-    // 닉네임 중복 확인
-    @GetMapping("/user/nickname/validate/{nickName}")
-    public ResponseEntity<Boolean> nickDoubleCheck(@PathVariable("nickName") String nickName) {
-        System.out.println("== nickName Double Check : " + nickName);
-        try {
-            signupService.nickDoubleCheck(nickName);
-            return new ResponseEntity<>(true, HttpStatus.OK);
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(false, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
     @PostMapping("/signup/{nickName}")
     public ResponseEntity<LoginUserDTO> signup(HttpServletRequest request, @PathVariable("nickName") String nickName) {
         try {
