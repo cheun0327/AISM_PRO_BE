f03342e33b5a1692e1f8a773c2a1f0045563e004
cheun0327 <cheun0327@gmail.com>
2021-11-03 18:58:40 +0900

    Fix:src image 파일에 업로드

--- src/main/java/com/upvote/aismpro/controller/UserController.java
+++ src/main/java/com/upvote/aismpro/controller/UserController.java
@@ -82,7 +82,7 @@ public class UserController {
         String imgFolder = userId.replaceAll("-","");
         String imgName = imgFolder + "." +imgNameArr[imgNameArr.length - 1];
 //        String dirPath = "/Users/upvote3/chaeeun/dev/react-workspace/AISM_PRO_FE/public/image/user/" + imgFolder;
-        String dirPath = "/var/lib/jenkins/workspace/AISM_PRO_REACT/public/image/user/" + imgFolder;
+        String dirPath = "/var/lib/jenkins/workspace/AISM_PRO_REACT/src/components/content/image/user/" + imgFolder;
         File profileDir = new File(dirPath);
 
 
