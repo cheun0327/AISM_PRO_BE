fd3463fa7945d7e42498ce7b21052c2346d2b1f3
cheun0327 <cheun0327@gmail.com>
2021-11-18 17:04:47 +0900

    Fix : delete all legacy

--- src/main/java/com/upvote/aismpro/entity/PlayListSongPK.java
+++
@@ -1,8 +0,0 @@
-package com.upvote.aismpro.entity;
-
-import java.io.Serializable;
-
-public class PlayListSongPK implements Serializable {
-    private String playlistId;
-    private String songId;
-}
--- src/main/java/com/upvote/aismpro/repository/PlaylistSongRepository.java
+++
@@ -1,8 +0,0 @@
-package com.upvote.aismpro.repository;
-
-import com.upvote.aismpro.customrepository.PlaylistSongRepositoryCustom;
-import com.upvote.aismpro.entity.PlayListSong;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistSongRepository extends JpaRepository<PlayListSong, String>, PlaylistSongRepositoryCustom {
-}
